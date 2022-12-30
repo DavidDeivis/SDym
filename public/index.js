7f4d01ea15817a3ac7d6d3a89c87435aed03f0c5
DavidDeivis <espontaneoclips@gmail.com>
2022-12-31 07:08:40 +0800

    ka

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ const containerEnd = document.getElementById("container-end");
 const button = document.getElementById("play");
 const soundInicial = document.querySelector(".audio")
 var desitionB = false;
-const https = "http://192.168.193.63:7535";
+const https = "https://staymlawter.onrender.com";
 // http://localhost:7535
 // https://staymlawter.onrender.com
 // http://192.168.193.63:7535
